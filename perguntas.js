22cbf191ec921ef1d6a8db305a76c2c6f641b2e6
FernandoGomesSilva <141933543+FernandoGomesSilva@users.noreply.github.com>
2024-11-07 23:56:37 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,53 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Química',
+    'O que são os hidrocarbonetos?',
+    'Hidrocarbonetos são compostos formados apenas por carbono e hidrogênio.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Química',
+    'Quais são os 4 tipos de hidrocarbonetos?',
+    'São eles: Alcano, alceno, alcino e alcadieno.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Química',
+    'Qual é o prefixo de um hidrocarboneto com 3 carbonos?',
+    'Seu prefixo é prop.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Química',
+    'Qual é o prefixo de um hidrocarboneto com 5 carbonos?',
+    'Seu prefixo é pent.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o prefixo de um hidrocarboneto com 6 carbonos?',
+    'Seu prefixo é Hex.'
+)
+
+criaCartao(
+    'Química',
+    'O que significa o infixo "an"?',
+    'Significa que o hidrocarboneto possui ligações simples entre carbonos.'
+)
+
+criaCartao(
+    'Química',
+    'O que siginifica o infixo "en"',
+    'Significa que o hidrocarboneto possui uma ligação dupla entre carbonos.'
+)
+
+criaCartao(
+    'Química',
+    'O que siginifica o infixo "in"',
+    'Significa que o hidrocarboneto possui uma ligação tripla entre carbonos.'
+)
+
+criaCartao(
+    'Química',
+    'O que é um hidrocarboneto ramificado?',
+    'É um hidrocarboneto cuja cadeia possui mais de duas extremidades'
 )
